6865355aac11fff6e16fb265b0179b48d1b739b1
George Zakharov <george_zakharov@mail.ru>
2022-04-06 01:18:34 +0300

    Simple login functions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,65 @@
-//
-// Created by g.zaharov on 06.04.2022.
-//
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+bool IsLoggedIn()
+{
+    string username, password, un, pw;
+
+    cout << "Enter username: ";
+    cin >> username;
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    ifstream read(username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+
+    if (un == username && pw == password) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Fix recursion
+int main()
+{
+    int choice;
+
+    cout << "1: Register\n2: Login\nYour choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        string username, password;
+        cout << "Select a username: ";
+        cin >> username;
+        cout << "Select a password: ";
+        cin >> password;
+
+        ofstream file(username + ".txt");
+        file << username << endl << password;
+        file.close();
+
+        main();
+    } else if (choice == 2) {
+        bool status = IsLoggedIn();
+
+        if (!status) {
+            cout << "False Login!" << endl;
+            system("PAUSE");
+            return 0;
+        } else {
+            cout << "Successfully logged in!";
+            system("PAUSE");
+            return 1;
+        }
+    } else {
+        cout << "Wrong choice!" << endl;
+        system("PAUSE");
+        return 0;
+    }
+}
